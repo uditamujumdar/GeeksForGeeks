0ac909d97b50ca3de7696a90bbf26282415b37b1
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-10-06 18:46:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse alternate nodes in Link List/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    struct Node* reverse(struct Node* head){
+        struct Node* curr=head;
+        struct Node* prev=NULL;
+        struct Node* fwd=NULL;
+        
+        while(curr!=NULL){
+            fwd=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=fwd;
+        }
+        
+        return prev;
+    }
+    
+    
+    void rearrange(struct Node *head)
+    {
+        if(head->next==NULL){
+            return ;
+        }
+
+        struct Node* curr=head;
+        struct Node* alt=head->next;
+        struct Node* head1=alt;
+        
+        while(curr->next!=NULL && alt->next!=NULL && alt!=NULL){
+            curr->next=curr->next->next;
+            curr=curr->next;
+            alt->next=alt->next->next;
+            alt=alt->next;
+        }
+        
+        curr->next=reverse(head1);
+        return ;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
