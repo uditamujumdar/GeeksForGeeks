ea4882786d386a780863892b247285e832f7825a
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-12-24 22:42:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    int n=grid.size();
+	    int m=grid[0].size();
+	    vector<vector<int>>vis(n, vector<int>(m, 0));
+	    vector<vector<int>>dist(n, vector<int>(m,0));
+	    queue<pair<pair<int, int>, int>>q;
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(grid[i][j]==1){
+	                vis[i][j]=1;
+	                q.push({{i,j}, 0});
+	            }
+	        }
+	    }
+	    
+	    int drow[]={-1,0,1,0};
+	    int dcol[]={0,1,0,-1};
+	    
+	    while(!q.empty()){
+	        int r=q.front().first.first;
+	        int c=q.front().first.second;
+	        int steps=q.front().second;
+	        q.pop();
+	        
+	        dist[r][c]=steps;
+	        
+	        for(int i=0;i<4;i++){
+	            int nrow=r+drow[i];
+	            int ncol=c+dcol[i];
+	            
+	            if(nrow>=0 && ncol>=0 && ncol<m && nrow<n && vis[nrow][ncol]==0){
+	                vis[nrow][ncol]=1;
+	                q.push({{nrow, ncol}, steps+1});
+	            }
+	        }
+	    }
+	    
+	    return dist;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.nearest(grid);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
