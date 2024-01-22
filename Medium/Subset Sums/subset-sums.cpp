30bac1743b3dc1c58cb590c313adb0ffc0875e83
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-01-22 18:48:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subset Sums/subset-sums.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> subsetSums(vector<int> arr, int n)
+    {
+        vector<int>ans;
+        ans.push_back(0);
+        
+        for (int i = 0; i <n; i++) {
+            int s = ans.size();
+            
+            for (int j= 0; j < s; j++) {
+                ans.push_back(ans[j] + arr[i]); 
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
