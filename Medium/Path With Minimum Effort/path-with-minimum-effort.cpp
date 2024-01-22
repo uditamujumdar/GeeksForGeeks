28ab8c39f75f9fe2fd4067fe22c3a13d84189347
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-01-22 19:45:04 +0530

    Updated Solution - GfG to GitHub

--- Medium/Path With Minimum Effort/path-with-minimum-effort.cpp
+++ Medium/Path With Minimum Effort/path-with-minimum-effort.cpp
@@ -6,14 +6,14 @@ using namespace std;
 class Solution {
   public:
     int MinimumEffort(vector<vector<int>>& heights) {
-        priority_queue<pair<int, pair<int, int>>,
-        vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
-        
         int n=heights.size();
         int m=heights[0].size();
         
-        vector<vector<int>>dist(n, vector<int>(m, 1e9));
+        priority_queue<pair<int, pair<int,int>>,vector<pair<int, pair<int,int>>>,
+                        greater<pair<int, pair<int,int>>>>pq;
+        
+        
+        vector<vector<int>>dist(n, vector<int>(m,1e9));
         dist[0][0]=0;
         pq.push({0,{0,0}});
         
@@ -21,29 +21,27 @@ class Solution {
         int dc[]={0,1,0,-1};
         
         while(!pq.empty()){
-            auto it= pq.top();
+            int diff=pq.top().first;
+            int r=pq.top().second.first;
+            int c=pq.top().second.second;
             pq.pop();
             
-            int diff=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
-            
-            if(row==n-1 && col==m-1){
+            if(r==n-1&&c==m-1){
                 return diff;
             }
             
             for(int i=0;i<4;i++){
-                int nr=row+dr[i];
-                int nc=col+dc[i];
-            
-            if(nr>=0 && nc>=0 && nc<m && nr<n){
-                int neweffort=max(abs(heights[row][col]-heights[nr][nc]), diff);
+                int nr=r+dr[i];
+                int nc=c+dc[i];
                 
-                if(neweffort<dist[nr][nc]){
-                    dist[nr][nc]=neweffort;
-                    pq.push({neweffort,{nr, nc}});
+                if(nr>=0&&nc>=0&&nr<n&&nc<m){
+                    int neweffort=max(abs(heights[nr][nc]-heights[r][c]), diff);
+                    
+                    if(neweffort<dist[nr][nc]){
+                        dist[nr][nc]=neweffort;
+                        pq.push({neweffort, {nr, nc}});
+                    }
                 }
-            }
             }
         }
         return 0;
