8f19b826ce2ef6e7b52659f5f18c2200fbc80ef0
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-01-28 23:49:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if a given graph is tree or not/check-if-a-given-graph-is-tree-or-not.cpp
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Matrix
+{
+public:
+    template <class T>
+    static void input(vector<vector<T>> &A,int n,int m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                scanf("%d ",&A[i][j]);
+            }
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<vector<T>> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            for (int j = 0; j < A[i].size(); j++)
+            {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+    class DisjointSet{
+    public:
+    vector<int>rank, parent, size;
+    DisjointSet(int n){
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+    class Solution {
+  public:
+    int isTree(int n, int m, vector<vector<int>> &adj) {
+        DisjointSet ds(n);
+        for(auto it: adj){
+            int u=it[0];
+            int v=it[1];
+            if(ds.findUParent(u)==ds.findUParent(v)) {
+                return 0;
+            }
+            
+            ds.unionBySize(u,v);
+        }
+        
+        int comp=0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i]==i){
+                comp++;
+            }
+        }
+        
+        return comp==1;
+        
+    }
+    };
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n; 
+        scanf("%d",&n);
+        
+        
+        int m; 
+        scanf("%d",&m);
+        
+        
+        vector<vector<int>> edges(m, vector<int>(2));
+        Matrix::input(edges,m,2);
+        
+        Solution obj;
+        int res = obj.isTree(n, m, edges);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
