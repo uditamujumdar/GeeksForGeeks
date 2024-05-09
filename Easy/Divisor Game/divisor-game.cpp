b397719f2ca910d1b1f28ffd70d0f9d0bd35453d
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-05-09 23:17:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Divisor Game/divisor-game.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool divisorGame(int n) {
+        if(n&1){
+            return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution obj;
+        bool ans = obj.divisorGame(n);
+        if (ans)
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
