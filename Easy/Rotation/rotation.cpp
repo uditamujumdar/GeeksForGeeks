80b221418ffffca3a7ad3267b76ae4467464734e
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-06-03 13:32:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotation/rotation.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+        int i=0,j=n-1;
+        int ans=INT_MAX;
+        int ind=-1;
+        
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[i]<=arr[mid]){
+                if(arr[i]<=ans){
+                    ans=arr[i];
+                    ind=i;
+                }
+                i=mid+1;
+            }
+            else{
+                if(arr[mid]<=ans){
+                    ans=arr[mid];
+                    ind=mid;
+                }
+                j=mid-1;
+            }
+        }
+        return ind;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
