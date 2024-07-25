fb1ff71a3ffe1bf312e2cbae972f133fd35e7155
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2024-07-25 20:55:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Page Faults in LRU/page-faults-in-lru.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int pageFaults(int n, int C, int pages[]){
+        int ans=0;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            auto it=find(v.begin(),v.end(),pages[i]);
+            if(it==v.end()){
+                if(v.size()==C){
+                    v.erase(v.begin());
+                }
+                ans++;
+                v.push_back(pages[i]);
+            }
+            else{
+                v.erase(it);
+                v.push_back(pages[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, C;
+        cin>>N;
+        int pages[N];
+        for(int i = 0;i < N;i++)
+            cin>>pages[i];
+        cin>>C;
+        
+        Solution ob;
+        cout<<ob.pageFaults(N, C, pages)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
